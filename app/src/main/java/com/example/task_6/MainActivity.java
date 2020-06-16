f02cc41e1243500c8f5bda67448bdc8ea4fb6dd3
TopHlop <45011539+TopHlop@users.noreply.github.com>
2020-06-16 16:14:43 +0400

    Defined basic camera settings.

--- app/src/main/java/com/example/task_6/MainActivity.java
+++ app/src/main/java/com/example/task_6/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.task_6;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
 import androidx.camera.core.Preview;
+import androidx.camera.extensions.HdrImageCaptureExtender;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -67,6 +71,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startCamera() {
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
+                ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindCamera(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
 
+    private void bindCamera(ProcessCameraProvider cameraProvider){
+        Preview preview = new Preview.Builder().build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                .build();
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder().build();
+        ImageCapture.Builder builder = new ImageCapture.Builder();
+
+        HdrImageCaptureExtender hdrImageCaptureExtender = HdrImageCaptureExtender.create(builder);
+        // Query if extension is available (optional).
+        if (hdrImageCaptureExtender.isExtensionAvailable(cameraSelector)) {
+            // Enable the extension if available.
+            hdrImageCaptureExtender.enableExtension(cameraSelector);
+        }
+
+        final ImageCapture imageCapture = builder
+                .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
+                .build();
+        cameraProvider.unbindAll();
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector,
+                preview, imageAnalysis, imageCapture);
+        preview.setSurfaceProvider(binding.previewView.createSurfaceProvider());
     }
 }
